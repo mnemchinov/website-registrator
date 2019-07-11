27c8fd06064d1883de806a9d2de231a48aaf9bb7
Михаил Немчинов <mnemchinov@mail.ru>
2019-07-11 17:26:46 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,282 @@
+function include(scriptUrl) {
+    document.write('<script src="' + scriptUrl + '"></script>');
+}
+
+function isIE() {
+    var myNav = navigator.userAgent.toLowerCase();
+    return (myNav.indexOf('msie') != -1) ? parseInt(myNav.split('msie')[1]) : false;
+};
+
+/* cookie.JS
+ ========================================================*/
+include('js/jquery.cookie.js');
+
+/* Easing library
+ ========================================================*/
+include('js/jquery.easing.1.3.js');
+
+
+
+/* ToTop
+ ========================================================*/
+;
+(function ($) {
+    var o = $('html');
+    if (o.hasClass('desktop')) {
+        include('js/jquery.ui.totop.js');
+
+        $(document).ready(function () {
+            $().UItoTop({easingType: 'easeOutQuart'});
+        });
+    }
+})(jQuery);
+
+/* EqualHeights
+ ========================================================*/
+;
+(function ($) {
+    var o = $('[data-equal-group]');
+    if (o.length > 0) {
+        include('js/jquery.equalheights.js');
+    }
+})(jQuery);
+
+/* SMOOTH SCROLLIG
+ ========================================================*/
+;
+(function ($) {
+    var o = $('html');
+    if (o.hasClass('desktop')) {
+        include('js/jquery.mousewheel.min.js');
+        include('js/jquery.simplr.smoothscroll.min.js');
+
+        $(document).ready(function () {
+            $.srSmoothscroll({
+                step: 150,
+                speed: 800
+            });
+        });
+    }
+})(jQuery);
+
+/* Copyright Year
+ ========================================================*/
+;
+(function ($) {
+    var currentYear = (new Date).getFullYear();
+    $(document).ready(function () {
+        $("#copyright-year").text((new Date).getFullYear());
+    });
+})(jQuery);
+
+/* Google Map
+ ========================================================*/
+;
+(function ($) {
+    var o = document.getElementById("google-map");
+    if (o) {
+        include('//maps.google.com/maps/api/js?sensor=false');
+        include('js/jquery.rd-google-map.js');
+
+        $(document).ready(function () {
+            var o = $('#google-map');
+            if (o.length > 0) {
+                o.googleMap({
+                    styles: [{"featureType":"administrative.locality","elementType":"all","stylers":[{"hue":"#2c2e33"},{"saturation":7},{"lightness":19},{"visibility":"on"}]},{"featureType":"landscape","elementType":"all","stylers":[{"hue":"#ffffff"},{"saturation":-100},{"lightness":100},{"visibility":"simplified"}]},{"featureType":"poi","elementType":"all","stylers":[{"hue":"#ffffff"},{"saturation":-100},{"lightness":100},{"visibility":"off"}]},{"featureType":"road","elementType":"geometry","stylers":[{"hue":"#bbc0c4"},{"saturation":-93},{"lightness":31},{"visibility":"simplified"}]},{"featureType":"road","elementType":"labels","stylers":[{"hue":"#bbc0c4"},{"saturation":-93},{"lightness":31},{"visibility":"on"}]},{"featureType":"road.arterial","elementType":"labels","stylers":[{"hue":"#bbc0c4"},{"saturation":-93},{"lightness":-2},{"visibility":"simplified"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"hue":"#e9ebed"},{"saturation":-90},{"lightness":-8},{"visibility":"simplified"}]},{"featureType":"transit","elementType":"all","stylers":[{"hue":"#e9ebed"},{"saturation":10},{"lightness":69},{"visibility":"on"}]},{"featureType":"water","elementType":"all","stylers":[{"hue":"#e9ebed"},{"saturation":-78},{"lightness":67},{"visibility":"simplified"}]}]
+                });
+            }
+        });
+    }
+})
+(jQuery);
+
+
+/* WOW
+ ========================================================*/
+;
+(function ($) {
+    var o = $('html');
+
+    if ((navigator.userAgent.toLowerCase().indexOf('msie') == -1 ) || (isIE() && isIE() > 9)) {
+        if (o.hasClass('desktop')) {
+            include('js/wow.js');
+
+            $(document).ready(function () {
+                new WOW().init();
+            });
+        }
+    }
+})(jQuery);
+
+/*
+/!* Contact Form
+ ========================================================*!/
+;
+(function ($) {
+    var o = $('#contact-form');
+    if (o.length > 0) {
+        include('js/modal.js');
+        include('js/TMForm.js'); 
+    }
+})(jQuery);
+*/
+
+/* Orientation tablet fix
+ ========================================================*/
+$(function () {
+    // IPad/IPhone
+    var viewportmeta = document.querySelector && document.querySelector('meta[name="viewport"]'),
+        ua = navigator.userAgent,
+
+        gestureStart = function () {
+            viewportmeta.content = "width=device-width, minimum-scale=0.25, maximum-scale=1.6, initial-scale=1.0";
+        },
+
+        scaleFix = function () {
+            if (viewportmeta && /iPhone|iPad/.test(ua) && !/Opera Mini/.test(ua)) {
+                viewportmeta.content = "width=device-width, minimum-scale=1.0, maximum-scale=1.0";
+                document.addEventListener("gesturestart", gestureStart, false);
+            }
+        };
+
+    scaleFix();
+    // Menu Android
+    if (window.orientation != undefined) {
+        var regM = /ipod|ipad|iphone/gi,
+            result = ua.match(regM);
+        if (!result) {
+            $('.sf-menus li').each(function () {
+                if ($(">ul", this)[0]) {
+                    $(">a", this).toggle(
+                        function () {
+                            return false;
+                        },
+                        function () {
+                            window.location.href = $(this).attr("href");
+                        }
+                    );
+                }
+            })
+        }
+    }
+});
+var ua = navigator.userAgent.toLocaleLowerCase(),
+    regV = /ipod|ipad|iphone/gi,
+    result = ua.match(regV),
+    userScale = "";
+if (!result) {
+    userScale = ",user-scalable=0"
+}
+document.write('<meta name="viewport" content="width=device-width,initial-scale=1.0' + userScale + '">');
+
+/* JQuery UI Accordion
+ ========================================================*/
+;
+(function ($) {
+    var o = $('.accordion');
+    if (o.length > 0) {
+        include('js/jquery.ui.accordion.min.js');
+
+        $(document).ready(function () {
+            o.accordion({
+                active: false,
+                header: '.accordion_header',
+                heightStyle: 'content',
+                collapsible: true
+            });
+        });
+    }
+})(jQuery);
+
+/**
+ * @module       RD Mailform
+ * @description  Enables RD Mailform Plugin
+ */
+;
+(function ($) {
+    var o = $('.rd-mailform');
+    if (o.length > 0) {
+        include('js/jquery.rd-mailform.js');
+
+        $(document).ready(function () {
+            var o = $('.rd-mailform');
+
+            if (o.length) {
+                o.rdMailForm({
+                    validator: {
+                        'constraints': {
+                            '@LettersOnly': {
+                                message: 'Please use letters only!'
+                            },
+                            '@NumbersOnly': {
+                                message: 'Please use numbers only!'
+                            },
+                            '@NotEmpty': {
+                                message: 'Field should not be empty!'
+                            },
+                            '@Email': {
+                                message: 'Enter valid e-mail address!'
+                            },
+                            '@Phone': {
+                                message: 'Enter valid phone number!'
+                            },
+                            '@Date': {
+                                message: 'Use MM/DD/YYYY format!'
+                            },
+                            '@SelectRequired': {
+                                message: 'Please choose an option!'
+                            }
+                        }
+                    }
+                }, {
+                    'MF000': 'Sent',
+                    'MF001': 'Recipients are not set!',
+                    'MF002': 'Form will not work locally!',
+                    'MF003': 'Please, define email field in your form!',
+                    'MF004': 'Please, define type of your form!',
+                    'MF254': 'Something went wrong with PHPMailer!',
+                    'MF255': 'There was an error submitting the form!'
+                });
+            }
+        });
+    }
+})(jQuery);
+
+/*/!* Subscribe Form
+ ========================================================*!/
+;
+(function ($) {
+    var o = $('#subscribe-form');
+    if (o.length > 0) {
+        include('js/sForm.js');
+    }
+})(jQuery);*/
+
+
+
+
+
+/* Booking Form
+ ========================================================*/
+;
+(function ($) {
+    var o = $('#bookingForm');
+    if (o.length > 0) {
+        include('js/booking/booking.js');
+        include('js/booking/jquery-ui-1.10.3.custom.min.js');
+        include('js/booking/jquery.fancyform.js');
+        include('js/booking/jquery.placeholder.js');
+        include('js/booking/regula.js');
+        $(document).ready(function () {
+            o.bookingForm({
+                ownerEmail: '#'
+            });
+        });
+    }
+})(jQuery);
+
+/* Parallax 
+=============================================*/ 
+;(function ($) { 
+    include('js/jquery.rd-parallax.js'); 
+})(jQuery);
